339271cba38fb16942428c1bf63df7ba05ede08f
ThouaibatSelim <selimthouaibat9@gmail.com>
2024-12-12 07:54:15 +0300

    dom

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 //Afficher année actuelle dans le footer
-function anneeFooter() { 
-  // get the the span element
-  const date = new Date();
-  // get the current year
-  const currentYear = date.getFullYear();
-  // set the year span element's text to the current year
-  document.querySelector("#footer").innerHTML = currentYear;
+const date = new Date();
+const elementDate = document.querySelector ("#currentyear");
+elementDate.textContent += `${date.getFullYear()} STF Mayotte. Tous droits réservés© `;
+
+// Règle validation formulaire
+const elementFormulaire = document.querySelector("#form");
+  console.log(elementFormulaire)
+
+//Récupérer
+function verifierchamp(champ) {
+  if(champ.value === "") {
+      console.log("le champ nom est vide");
+  } else {
+      console.log("le champ nom est rempli");
+  }
 };
+
+//Nom
+elementFormulaire.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const elementInputNom = document.querySelector("#nom");
+    const valueNom = elementInputNom.value;
+    console.log("##### Nom saisi:", valueNom)
+    const elementInputPrenom = document.querySelector("#prenom");
+    verifierchamp(elementInputNom);
+    verifierchamp(elementInputPrenom);
+});
